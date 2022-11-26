b4e167ce692498f07e70a8e30c9c8526185a1ced
Andreas Naumann <andreas.naumann@mathematik.tu-chemnitz.de>
2022-11-26 19:24:55 +0100

    adds the test data for source_withQ

---
+++ source_withQ/reference.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def getReference(x0):
+  l = 10.0
+  sCoeffs = [0.0, 20]
+  yRef = np.polynomial.Polynomial(sCoeffs)
+  f = lambda t, y: l*yRef(t)
+  sol = (l*yRef).integ(k=[x0])
+  return (f, sol, yRef)
