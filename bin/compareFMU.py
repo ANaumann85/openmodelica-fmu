c1a6fd1c39943fcacae24da3f5935afe5c7fb575
Andreas Naumann <andreas.naumann@mathematik.tu-chemnitz.de>
2022-11-26 19:20:41 +0100

    adds the comparison scripts

---
+++ bin/compareFMU.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+import importlib.util
+import numpy as np
+from scipy import integrate
+import fmpy
+from tabulate import tabulate
+
+def getFMUSol(nInter, Tref1, x0, fmuFile):
+  outputs = ['time', 'pipe1Q', 'Tref1', 'T', 'der(T)']
+  tEnd = 0.2
+  inSigDtype = [('time', np.double), ('Tref1', np.double)]
+  sigTime = np.linspace(0, tEnd, nInter)
+  inSig = np.array([ (t, Tref1(t)) for t in sigTime], dtype=inSigDtype) # piecewise linear
+  resFMU = fmpy.simulate_fmu(fmuFile,start_values={'T0' : x0 }, start_time=0.0, stop_time=tEnd, step_size=0.02, input=inSig, output=outputs, output_interval=tEnd/nInter)
+  return resFMU
+
+def compare(problem, fmuFile):
+  x0 = 5.0
+  _, sol, Tref1 = problem(x0)
+  diff = []
+  for k in range(10):
+    nInter = 10*(2**k)
+    fmuSol = getFMUSol(nInter, Tref1, x0, fmuFile)
+    refSol = sol(fmuSol['time'])
+    curDiff = float(np.max(np.abs(fmuSol['T']-refSol)))
+    diff.append((nInter, curDiff))
+  print(tabulate(diff, headers=['nInter', 'maxDiff']))
+
+if __name__ == '__main__':
+  parser = ArgumentParser('compares the results from an FMU (in cs mode) with analytic solutions')
+  parser.add_argument('problemDir', help='the problem folder')
+  parser.add_argument('--fmuFile', default='testModel_cs.fmu', help='fmu file name in the folder')
+  parser.add_argument('--refFunc', default='getReference', help='name of the reference solution')
+  args = parser.parse_args()
+  spec = importlib.util.spec_from_file_location('reference', f'{args.problemDir}/reference.py')
+  module = importlib.util.module_from_spec(spec)
+  spec.loader.exec_module(module)
+  fmuFile = f'{args.problemDir}/{args.fmuFile}'
+  refFunc = getattr(module, args.refFunc)
+  compare(refFunc, fmuFile)
+# vim:set et sw=2 ts=2:
---
+++ bin/compareModelica.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import argparse
+import importlib.util
+import numpy as np
+import scipy.io as sio
+
+def getValue(varNr):
+  datArray = resData['dataInfo'][0][varNr]
+  if datArray == 0:
+    datArray = 2 # time is in the second data
+  datArray = f'data_{datArray}'
+  datIndex = resData['dataInfo'][1][varNr]-1
+  return resData[datArray][datIndex]
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='plot a selection of modelica results')
+  parser.add_argument('problemDir', help='the problem folder')
+  parser.add_argument('--resFile', help='name of the result file', default='SimModel_res.mat')
+  parser.add_argument('--solName', help='name of the solution variable', default='testModel.T')
+  parser.add_argument('--refFunc', default='getReference', help='name of the reference solution')
+
+  args = parser.parse_args()
+  resFile = f'{args.problemDir}/{args.resFile}'
+  resData = sio.loadmat(resFile, matlab_compatible=True)
+  names = resData['name']
+  names = [''.join(names[:,k]) for k in range(names.shape[1])]
+  solIndex = names.index(args.solName)
+  timeIndex = 0
+  times = getValue(timeIndex)
+  fmuSol = getValue(solIndex)
+  spec = importlib.util.spec_from_file_location('reference', f'{args.problemDir}/reference.py')
+  module = importlib.util.module_from_spec(spec)
+  spec.loader.exec_module(module)
+  refFunc = getattr(module, args.refFunc)
+  rhs, sol, Tref1 = refFunc(5.0)
+  refSol = sol(times)
+  maxDiff = np.max(np.abs(refSol-fmuSol))
+  print(f'maxDiff: {maxDiff}')
+
+# vim:set et sw=2 ts=2:
